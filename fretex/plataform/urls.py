9ffe3f9517538539dd9566cbfff13bb6905184bf
Lucas-Veras <verasluca00@gmail.com>
2022-06-21 23:35:24 -0300

    cadastros e login adicionados

--- fretex/plataform/urls.py
+++ fretex/plataform/urls.py
@@ -4,4 +4,8 @@ from . import views
 
 urlpatterns = [
     path('', views.landing),
+    path('login/', views.login, name='login'),
+    path('escolhacadastro/', views.escolhaCadastro, name='escolhacadastro'),
+    path('cadastrocliente/', views.cadastroCliente, name='cadastrocliente'),
+    path('cadastrofreteiro/', views.cadastroFreteiro, name='cadastrofreteiro')
 ]
--- fretex/plataform/views.py
+++ fretex/plataform/views.py
@@ -3,3 +3,15 @@ from django.shortcuts import render
 # Create your views here.
 def landing(request):
     return render(request, 'landing.html')
+
+def login(request):
+    return render(request, 'login.html')
+
+def escolhaCadastro(request):
+    return render(request, 'escolhaCadastro.html')
+
+def cadastroCliente(request):
+    return render(request, 'cadastroCliente.html')
+
+def cadastroFreteiro(request):
+    return render(request, 'cadastroFreteiro.html')
