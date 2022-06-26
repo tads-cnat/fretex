67c7bd89c5dcc94062e1a8c8529677e9a1345e79
Mathews Dantas <94187504+MathewsDantas@users.noreply.github.com>
2022-06-25 22:16:40 -0300

    Adição das classes de modelo

--- fretex/plataform/admin.py
+++ fretex/plataform/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Cliente, Endereco, Freteiro, Pedido, Produto, Proposta, Status, TipoVeiculo, Usuario, Veiculo
 
-# Register your models here.
+admin.site.register(Cliente)
+admin.site.register(Freteiro)
+admin.site.register(Pedido)
+admin.site.register(Proposta)
+admin.site.register(Veiculo)
+admin.site.register(TipoVeiculo)
+admin.site.register(Produto)
+admin.site.register(Status)
+admin.site.register(Endereco)
