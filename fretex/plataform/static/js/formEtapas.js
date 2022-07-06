478f4d9594f63f51a699040f7aa5c557eaeca142
Lucas-Veras <verasluca00@gmail.com>
2022-07-06 20:56:58 -0300

    added dashboard cliente

--- fretex/plataform/static/js/formEtapas.js
+++ fretex/plataform/static/js/formEtapas.js
@@ -2,18 +2,32 @@ function formEtapas(){
     const passos = document.querySelectorAll(".step");
     const proximo = document.querySelectorAll("#next");
     const anterior = document.querySelectorAll("#prev");
-
+    const marginBottomForm = document.querySelector("#marginBottomForm");
     if(passos.length && proximo.length && anterior.length){
         passos[0].classList.add("ativo");
 
         function proximoForm(index){
             passos[index].classList.remove("ativo");
             passos[index + 1].classList.add("ativo");
+            if(index + 1 == passos.length - 1){
+                marginBottomForm.classList.remove("md:mb-5");
+                marginBottomForm.classList.add("md:mb-64");
+            } else{
+                marginBottomForm.classList.remove("md:mb-64");
+                marginBottomForm.classList.add("md:mb-5");
+            }
         }
 
         function formAnterior(index){
             passos[index+1].classList.remove("ativo");
             passos[index].classList.add("ativo");
+            if(index + 1 == passos.length - 1){
+                marginBottomForm.classList.remove("md:mb-5");
+                marginBottomForm.classList.add("md:mb-64");
+            } else{
+                marginBottomForm.classList.remove("md:mb-64");
+                marginBottomForm.classList.add("md:mb-5");
+            }
         }
 
         passos.forEach((passo, index) => {
