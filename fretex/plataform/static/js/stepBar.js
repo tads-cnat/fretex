d8f2ddbb5ed46a4a089ea839fa6028663dfe6084
Lucas-Veras <verasluca00@gmail.com>
2022-07-25 21:31:07 -0300

    added userValidation

--- fretex/plataform/static/js/stepBar.js
+++
@@ -1,8 +0,0 @@
-const barItems = document.querySelectorAll(".stepBar-item");
-const etapasForm = document.querySelectorAll("#step");
-
-if (barItems.length && etapasForm.length) {
-    etapasForm.forEach((etapa, index) => {
-
-    })
-}
