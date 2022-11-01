e2821b6d499d46aca519a69b12983dc63ca00e94
Lucas-Veras <verasluca00@gmail.com>
2022-10-31 22:21:56 -0300

    pequenos ajustes footer

--- frontend/src/components/Footer/styles.tsx
+++ frontend/src/components/Footer/styles.tsx
@@ -1,62 +1,61 @@
 import styled from "styled-components";
 
-export const FooterStyled = styled.footer`
-    display: grid;
-    grid-template-columns: 1fr 1fr 1fr;
-    gap: 5rem;
-    background-color: var(--bg-grey);
+export const FinalFooterContainer = styled.section`
+  background-color: var(--bg-grey2);
+  padding: 25px 0;
+`;
 
-    @media (max-width: 1200px) {
-        gap: 2rem;
+export const FooterContainer = styled.section`
+  background-color: var(--bg-grey);
+`;
+
+export const FooterStyled = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr;
+  gap: 2rem;
+  padding: 40px 0;
+
+  @media (max-width: 768px) {
+    grid-template-columns: 1fr;
+    gap: 1rem;
   }
+`;
 
-    @media (max-width: 768px) {
-        grid-template-columns: 1fr;
-        justify-items: center;
+export const Logos = styled.div`
+  display: flex;
+  gap: 1.5rem;
+  margin-top: 64px;
+
+  @media (max-width: 768px) {
+    margin-top: 16px;
   }
 `;
 
-export const FooterContainer = styled.footer`
-
+export const FooterRodaPe = styled.p`
+  color: var(--text-grey-2);
 `;
 
-export const Logos = styled.footer`
-    margin-top: 64px;
-    display: flex;
-    gap: 1.5rem;
+export const FooterBoxLogo = styled.div``;
 
-`;
+export const FooterBoxInfo = styled.div``;
 
-export const FooterRodaPe = styled.footer`
-    display: flex;
-    align-items: center;
-    justify-content: left;
-    padding-left: 207px;
-    color: var(--text-grey-2);
-    background-color: var(--bg-grey2);
-`;
-
-export const FooterBoxLogo = styled.footer`
-
-`;
-
-export const FooterBoxInfo = styled.footer`
-
-
-`;
 export const Logo = styled.h1`
-    font-family: "Poppins", sans-serif;
-    font-size: var(--font-xxl);
-    color: var(--text-light);
+  font-family: "Poppins", sans-serif;
+  font-size: var(--font-xxl);
+  color: var(--text-light);
 `;
 
 export const Title = styled.h2`
-    margin-bottom: 32px;
-    font-size: var(--font-large);
-    color: var(--text-light);
+  margin-bottom: 32px;
+  font-size: var(--font-large);
+  color: var(--text-light);
+
+  @media (max-width: 768px) {
+    margin-bottom: 16px;
+  }
 `;
 
 export const Info = styled.p`
-    font-size: var(--font-medium);
-    color: var(--text-grey-1);
+  font-size: var(--font-medium);
+  color: var(--text-grey-1);
 `;
