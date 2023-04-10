ab732b3126d1f4322c1edb8d9f46c1410e0c6a0e
ItaloGSM <italogabriel1aaaa@gmail.com>
2023-04-10 10:31:04 -0300

    correção: corrigidos os metodos de verificação dos campos unique de email, cpf e url_foto com o metodo hasOwnProperty

--- frontend/src/hooks/useFreteiroForm.ts
+++ frontend/src/hooks/useFreteiroForm.ts
@@ -1,10 +1,10 @@
-import { useState } from "react";
-import { type IFreteiroFormData } from "../interfaces";
-import useApi from "./useApi";
-import { type SubmitHandler } from "react-hook-form";
+import { useState } from 'react';
+import { type IFreteiroFormData } from '../interfaces';
+import useApi from './useApi';
+import { type SubmitHandler } from 'react-hook-form';
 
 interface Props {
-  onSuccess: () => void
+  onSuccess: () => void;
 }
 
 interface IUseFreteiroForm {
@@ -13,13 +13,13 @@ interface IUseFreteiroForm {
 }
 
 export const useFreteiroForm = ({ onSuccess }: Props): IUseFreteiroForm => {
-  const { registerFreteiro } = useApi()
-  const [error, setError] = useState("")
+  const { registerFreteiro } = useApi();
+  const [error, setError] = useState('');
 
   const onSubmit: SubmitHandler<IFreteiroFormData> = (data) => {
     if (data.url_foto.length === 0) {
-      setError("Imagem obrigatória!")
-      return
+      setError('Imagem obrigatória!');
+      return;
     }
     const formData = new FormData();
     const { endereco, ...freteiro } = data;
@@ -34,21 +34,28 @@ export const useFreteiroForm = ({ onSuccess }: Props): IUseFreteiroForm => {
     registerFreteiro(formData)
       .then(onSuccess)
       .catch((err) => {
-        const errors = err.response.data.errors
-        if (errors.hasOwnProperty('email') && errors.email[0] === 'This field must be unique.') {
-          setError("Email, possui uma conta cadastrada!")
-        }
-        else if (errors.hasOwnProperty('cpf') && errors.cpf[0] === 'This field must be unique.') {
-          setError("CPF, possui uma conta cadastrada!")
-        }
-        else if (errors.hasOwnProperty('url_foto') && errors.url_foto[0].includes('Upload a valid image.')) {
-          setError("Envie uma imagem válida!")
+        const errors = err.response.data.errors;
+        if (
+          Object.prototype.hasOwnProperty.call(errors, 'email') &&
+          errors.email[0] === 'This field must be unique.'
+        ) {
+          setError('Email, possui uma conta cadastrada!');
+        } else if (
+          Object.prototype.hasOwnProperty.call(errors, 'cpf') &&
+          errors.cpf[0] === 'This field must be unique.'
+        ) {
+          setError('CPF, possui uma conta cadastrada!');
+        } else if (
+          Object.prototype.hasOwnProperty.call(errors, 'url_foto') &&
+          errors.url_foto[0].includes('Upload a valid image.')
+        ) {
+          setError('Envie uma imagem válida!');
         }
       });
   };
 
   return {
     onSubmit,
-    error
-  }
-}
+    error,
+  };
+};
