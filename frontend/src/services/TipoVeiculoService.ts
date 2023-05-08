e3d6b71ad92354da58ac5fc1c5280edd36750070
Tonny-Weslley <tonnyweslleyss@gmail.com>
2023-05-07 23:18:48 -0300

    refactor: adicionando classe de serviços de TipoVeiculoService

---
+++ frontend/src/services/TipoVeiculoService.ts
@@ -0,0 +1,7 @@
+import BaseService from './BaseService';
+import { api } from '../api/api';
+
+class TipoVeiculoService extends BaseService {
+}
+
+export default new TipoVeiculoService('/tipodeveiculo');
