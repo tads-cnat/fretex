995abf1d2ea728632e0cfe945f1059290457f162
Tonny-Weslley <tonnyweslleyss@gmail.com>
2023-05-10 22:42:08 -0300

    style: padronizando novos arquivos do projeto

--- frontend/src/services/PropostaService.ts
+++ frontend/src/services/PropostaService.ts
@@ -1,16 +1,16 @@
 import BaseService from "./BaseService";
-import {api} from '../api/api'
+import {api} from '../api/api';
 
 class PropostaService extends BaseService {
 
     async registerProposta (proposta: FormData){
-        const response = await this.post(proposta)
-        return response
-    }
+        const response = await this.post(proposta);
+        return response;
+    };
 
     async getPropostasForPedido (queryString: any){
-        const response = await api.get(this.serviceUrl +'/?' + queryString)
-        return response
-    }
+        const response = await api.get(this.serviceUrl +'/?' + queryString);
+        return response;
+    };
 
-}
+};
