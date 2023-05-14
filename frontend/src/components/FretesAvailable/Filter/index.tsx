2c2014cacb94937e3fd101eb438a909a84380a7a
Mathews Dantas <94187504+MathewsDantas@users.noreply.github.com>
2023-05-13 21:53:19 -0300

    refactor: filtragem de tiposVeiculs agora está sendo puxada da API

--- frontend/src/components/FretesAvailable/Filter/index.tsx
+++ frontend/src/components/FretesAvailable/Filter/index.tsx
@@ -1,3 +1,5 @@
+import { useQuery } from 'react-query';
+import useApi from '../../../hooks/useApi';
 import { ContainerFilter, TypesVehicles, PeriodCollect } from './styles';
 
 interface IFilterFretes {
@@ -11,30 +13,30 @@ const Filter = ({
   veiculos,
   handleChange,
 }: IFilterFretes): JSX.Element => {
+  const { tiposVeiculo } = useApi();
+  const { data: tiposVeiculos } = useQuery(
+    'tiposVeiculosDisponiveis',
+    tiposVeiculo,
+  );
+
   return (
     <ContainerFilter>
       <TypesVehicles>
         <h2>Tipos de Veículo</h2>
-        <label>
-          <input
-            type="checkbox"
-            name="carro"
-            value="1"
-            checked={veiculos.includes('1')}
-            onChange={(e) => handleChange(e, 'veiculo')}
-          />
-          <span>Carro</span>
-        </label>
-        <label>
-          <input
-            type="checkbox"
-            name="caminhao"
-            value="2"
-            checked={veiculos.includes('2')}
-            onChange={(e) => handleChange(e, 'veiculo')}
-          />
-          <span>Caminhão</span>
-        </label>
+        {tiposVeiculos?.data.map((tipo) => (
+          <label key={tipo.id}>
+            <input
+              type="checkbox"
+              name={tipo.descricao}
+              value={tipo.id}
+              checked={veiculos.includes(`${tipo.id}`)}
+              onChange={(e) => {
+                handleChange(e, 'veiculo');
+              }}
+            />
+            <span>{tipo.descricao}</span>
+          </label>
+        ))}
       </TypesVehicles>
       <PeriodCollect>
         <h2>Turno de coleta</h2>
@@ -44,7 +46,9 @@ const Filter = ({
             name="matutino"
             value="MA"
             checked={coleta.includes('MA')}
-            onChange={(e) => handleChange(e, 'coleta')}
+            onChange={(e) => {
+              handleChange(e, 'coleta');
+            }}
           />
           <span>Matutino</span>
         </label>
@@ -54,7 +58,9 @@ const Filter = ({
             name="vespertino"
             value="TA"
             checked={coleta.includes('TA')}
-            onChange={(e) => handleChange(e, 'coleta')}
+            onChange={(e) => {
+              handleChange(e, 'coleta');
+            }}
           />
           <span>Vespertino</span>
         </label>
@@ -64,7 +70,9 @@ const Filter = ({
             name="noturno"
             value="NO"
             checked={coleta.includes('NO')}
-            onChange={(e) => handleChange(e, 'coleta')}
+            onChange={(e) => {
+              handleChange(e, 'coleta');
+            }}
           />
           <span>Noturno</span>
         </label>
