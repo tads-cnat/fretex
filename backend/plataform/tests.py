6c3a633092fc4a70a30257af01c8370a3d44d733
Mathews Dantas <94187504+MathewsDantas@users.noreply.github.com>
2023-06-20 19:55:03 -0300

    feat: test de cadastro de cliente

--- backend/plataform/tests.py
+++ backend/plataform/tests.py
@@ -1,5 +1,5 @@
 from django.urls import get_resolver, reverse
-from plataform.models import Freteiro
+from plataform.models import Cliente, Freteiro
 from rest_framework import status
 from rest_framework.test import APITestCase
 
@@ -28,3 +28,18 @@ class FreteiroTests(APITestCase):
         response = self.client.post(url, data, format="json")
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
         self.assertEqual(Freteiro.objects.filter(cpf=data["cpf"]).exists(), True)
+
+class ClienteTests(APITestCase):
+    def test_create_cliente(self):
+
+        url = reverse("auth-register-cliente")
+        data = {
+            "full_name": "Mathews Dantas Bezerra dos Santos",
+            "email": "cliente@hotmail.com",
+            "cpf": "11945011614",
+            "password": "Tads.d.b.s123",
+        }
+        response = self.client.post(url, data, format="json")
+        print('status test_create_cliente:',response.status_code)
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(Cliente.objects.filter(cpf=data["cpf"]).exists(), True)
