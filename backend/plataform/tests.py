11f127d05e5fa1e568cfab09881cc3e67dbc50f8
Italo Gabriel da Silva Monteiro <20211014040019@ifrn.local>
2023-06-21 13:45:26 -0300

    caso de teste delete e update

--- backend/plataform/tests.py
+++ backend/plataform/tests.py
@@ -4,6 +4,8 @@ from django.urls import reverse
 from plataform.models import Cliente, Freteiro, TipoVeiculo
 from rest_framework import status
 from rest_framework.test import APITestCase
+from django.test import TestCase
+
 
 
 class FreteiroTests(APITestCase):
@@ -42,10 +44,11 @@ class ClienteTests(APITestCase):
             "password": "Tads.d.b.s123",
         }
         response = self.client.post(url, data, format="json")
+        print('status test_create_cliente:',response.status_code)
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
         self.assertEqual(Cliente.objects.filter(cpf=data["cpf"]).exists(), True)
 
-class CadastroTipoVeiculoSistema(APITestCase):
+class CadastroTipoVeiculoTests(APITestCase):
     def test_create_tipo_veiculo(self):
 
         #CRIANDO FRETEIRO
@@ -83,15 +86,62 @@ class CadastroTipoVeiculoSistema(APITestCase):
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
         self.assertEqual(TipoVeiculo.objects.filter(descricao=dataTipoVeiculo["descricao"]).exists(), True)
 
-class CadastroTipoVeiculoIntegracao(APITestCase):
+
+class CadastroTipoVeiculoTestsSistema(APITestCase):
     def test_create_tipo_veiculo(self):
 
-        TipoVeiculo.objects.create(
-            descricao = "caminhao 3/4"
-        )
+        #CRIANDO FRETEIRO
+        urlFreteiro = reverse("auth-register-freteiro")
+        dataFreteiro = {
+            "full_name": "Italo Gabriel Teste",
+            "cpf": "12345678910",
+            "email": "teste@gmail.com",
+            "password": "123456",
+            "endereco": {
+                "rua": "Rua Teste",
+                "CEP": "12345678",
+                "numero": "123",
+                "bairro": "Bairro Teste",
+                "cidade": "Cidade Teste",
+                "estado": "Estado Teste",
+                "complemento": "Complemento Teste",
+            },
+        }
+        self.client.post(urlFreteiro, dataFreteiro, format="json")
+        #RECUPERA O USER CONTIDO NO FRETEIRO
+        user = User.objects.get(username='teste@gmail.com')
+        freteiro = Freteiro.objects.get(username='teste@gmail.com')
 
-        print('TipoVeiculo: ', TipoVeiculo.objects.filter(descricao='caminhao 3/4').first())
+        #AUTENTICA O USER DO FRETEIRO
+        self.client.force_authenticate(user=user)
 
-        tipoDeVeiculoExists = TipoVeiculo.objects.filter(descricao='caminhao 3/4').exists()
-        self.assertEqual(tipoDeVeiculoExists, True)
+        #CRIA O TIPO VEICULO
+        urlTipoVeiculo = reverse("tipodeveiculo-list")
+        dataTipoVeiculo = {
+          "descricao": "Caminhão 3/4",
+        }
+
+        response = self.client.post(urlTipoVeiculo, dataTipoVeiculo, format="json")
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(TipoVeiculo.objects.filter(descricao=dataTipoVeiculo["descricao"]).exists(), True)
+        
+
+class CadastroTipoVeiculoIntegracao(TestCase):
+    def setUp(self):
+        self.tipo_veiculo = TipoVeiculo.objects.create(descricao="caminhao 3/4")
+
+    def test_update_tipo_veiculo(self):
+        tipo_veiculo = TipoVeiculo.objects.get(descricao="caminhao 3/4")
+        tipo_veiculo.descricao = "moto"
+        tipo_veiculo.save()
+
+        updated_tipo_veiculo = TipoVeiculo.objects.get(pk=self.tipo_veiculo.pk)
+        self.assertEqual(updated_tipo_veiculo.descricao, "moto")
+
+    def test_delete_tipo_veiculo(self):
+        tipo_veiculo = TipoVeiculo.objects.get(descricao="caminhao 3/4")
+        tipo_veiculo.delete()
+
+        tipo_veiculo_exists = TipoVeiculo.objects.filter(descricao="caminhao 3/4").exists()
+        self.assertFalse(tipo_veiculo_exists)
 
