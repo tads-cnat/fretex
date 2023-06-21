e203c4cf7aa815ba0f5ca7492a2b87b543c9824c
ArthurOnly <arthurmedeirospaiva56@gmail.com>
2023-06-21 13:28:50 -0300

    test: teste de integração cadastro freteiro

--- backend/plataform/tests.py
+++ backend/plataform/tests.py
@@ -1,13 +1,13 @@
 
 from django.contrib.auth.models import User
 from django.urls import reverse
-from plataform.models import Cliente, Freteiro, TipoVeiculo
+from plataform.models import Cliente, Freteiro, TipoVeiculo, Endereco
 from rest_framework import status
 from rest_framework.test import APITestCase
 
 
 class FreteiroTests(APITestCase):
-    def test_create_freteiro(self):
+    def test_create_freteiro_sistema(self):
         """
         Ensure we can create a new freteiro.
         """
@@ -30,6 +30,28 @@ class FreteiroTests(APITestCase):
         response = self.client.post(url, data, format="json")
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
         self.assertEqual(Freteiro.objects.filter(cpf=data["cpf"]).exists(), True)
+
+    def test_create_freteiro_integracao(self):
+        endereco = Endereco.objects.create(
+            rua="Rua Teste",
+            CEP="12345678",
+            numero="123",
+            bairro="Bairro Teste",
+            cidade="Cidade Teste",
+            estado="Estado Teste",
+            complemento="Complemento Teste",
+        )
+        freteiro = Freteiro.objects.create(
+            username="ArthurPaiva",
+            first_name="Arthur",
+            last_name="Paiva",
+            cpf="12345678910",
+            email="teste@gmail.com",
+            endereco=endereco
+        )
+        freteiro_created = Freteiro.objects.filter(username="ArthurPaiva").exists()
+        self.assertEqual(freteiro_created, True)
+
 
 class ClienteTests(APITestCase):
     def test_create_cliente(self):
