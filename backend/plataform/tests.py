558d0accda3f68672b2910fab69856f6dfa5e29f
ItaloGSM <italogabriel1aaaa@gmail.com>
2023-06-21 10:19:11 -0300

    test
    teste de integração tipo_veiculo

--- backend/plataform/tests.py
+++ backend/plataform/tests.py
@@ -1,5 +1,7 @@
-from django.urls import get_resolver, reverse
-from plataform.models import Cliente, Freteiro
+
+from django.contrib.auth.models import User
+from django.urls import reverse
+from plataform.models import Cliente, Freteiro, TipoVeiculo
 from rest_framework import status
 from rest_framework.test import APITestCase
 
@@ -43,3 +45,41 @@ class ClienteTests(APITestCase):
         print('status test_create_cliente:',response.status_code)
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
         self.assertEqual(Cliente.objects.filter(cpf=data["cpf"]).exists(), True)
+
+class CadastroTipoVeiculoTests(APITestCase):
+    def test_create_tipo_veiculo(self):
+
+        #CRIANDO FRETEIRO
+        urlFreteiro = reverse("auth-register-freteiro")
+        dataFreteiro = {
+            "full_name": "Italo Gabriel Teste",
+            "cpf": "12345678910",
+            "email": "teste@gmail.com",
+            "password": "123456",
+            "endereco": {
+                "rua": "Rua Teste",
+                "CEP": "12345678",
+                "numero": "123",
+                "bairro": "Bairro Teste",
+                "cidade": "Cidade Teste",
+                "estado": "Estado Teste",
+                "complemento": "Complemento Teste",
+            },
+        }
+        self.client.post(urlFreteiro, dataFreteiro, format="json")
+        #RECUPERA O USER CONTIDO NO FRETEIRO
+        user = User.objects.get(username='teste@gmail.com')
+        freteiro = Freteiro.objects.get(username='teste@gmail.com')
+
+        #AUTENTICA O USER DO FRETEIRO
+        self.client.force_authenticate(user=user)
+
+        #CRIA O TIPO VEICULO
+        urlTipoVeiculo = reverse("tipodeveiculo-list")
+        dataTipoVeiculo = {
+          "descricao": "Caminhão 3/4",
+        }
+
+        response = self.client.post(urlTipoVeiculo, dataTipoVeiculo, format="json")
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(TipoVeiculo.objects.filter(descricao=dataTipoVeiculo["descricao"]).exists(), True)
