c56c795fc5e2fb810343b8a8bf78718f335d0a87
Italo Gabriel da Silva Monteiro <20211014040019@ifrn.local>
2023-06-21 13:26:32 -0300

    sintaxe

--- backend/plataform/tests.py
+++ backend/plataform/tests.py
@@ -5,7 +5,48 @@ from plataform.models import Cliente, Freteiro, TipoVeiculo
 from rest_framework import status
 from rest_framework.test import APITestCase
 
-class CadastroTipoVeiculoTestsSistema(APITestCase):
+
+class FreteiroTests(APITestCase):
+    def test_create_freteiro(self):
+        """
+        Ensure we can create a new freteiro.
+        """
+        url = reverse("auth-register-freteiro")
+        data = {
+            "full_name": "Arthur Paiva Teste",
+            "cpf": "12345678910",
+            "email": "teste@gmail.com",
+            "password": "123456",
+            "endereco": {
+                "rua": "Rua Teste",
+                "CEP": "12345678",
+                "numero": "123",
+                "bairro": "Bairro Teste",
+                "cidade": "Cidade Teste",
+                "estado": "Estado Teste",
+                "complemento": "Complemento Teste",
+            },
+        }
+        response = self.client.post(url, data, format="json")
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(Freteiro.objects.filter(cpf=data["cpf"]).exists(), True)
+
+class ClienteTests(APITestCase):
+    def test_create_cliente(self):
+
+        url = reverse("auth-register-cliente")
+        data = {
+            "full_name": "Mathews Dantas Bezerra dos Santos",
+            "email": "cliente@hotmail.com",
+            "cpf": "11945011614",
+            "password": "Tads.d.b.s123",
+        }
+        response = self.client.post(url, data, format="json")
+        print('status test_create_cliente:',response.status_code)
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(Cliente.objects.filter(cpf=data["cpf"]).exists(), True)
+
+class CadastroTipoVeiculoSistema(APITestCase):
     def test_create_tipo_veiculo(self):
 
         #CRIANDO FRETEIRO
@@ -43,7 +84,7 @@ class CadastroTipoVeiculoTestsSistema(APITestCase):
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
         self.assertEqual(TipoVeiculo.objects.filter(descricao=dataTipoVeiculo["descricao"]).exists(), True)
 
-class CadastroTipoVeiculoTestsIntegração(APITestCase):
+class CadastroTipoVeiculoIntegração(APITestCase):
     def test_create_tipo_veiculo(self):
 
         TipoVeiculo.objects.create(
@@ -54,3 +95,4 @@ class CadastroTipoVeiculoTestsIntegração(APITestCase):
 
         tipoDeVeiculoExists = TipoVeiculo.objects.filter(descricao='caminhao 3/4').exists()
         self.assertEqual(tipoDeVeiculoExists, True)
+
