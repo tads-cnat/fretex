4154f57e11b598406e76628a44f5c1607c54bad6
Arthur Paiva <arthur.paiva@lais.huol.ufrn.br>
2023-07-05 11:31:18 -0300

    feat: testes de sistema freteiro

--- backend/plataform/tests.py
+++ backend/plataform/tests.py
@@ -1,14 +1,34 @@
+from datetime import datetime
 
 from django.contrib.auth.models import User
 from django.urls import reverse
-from plataform.models import (Cliente, Endereco, Freteiro, Pedido, Produto, TipoVeiculo)
+from plataform.models import Cliente, Endereco, Freteiro, Pedido, Produto, TipoVeiculo
 from rest_framework import status
 from rest_framework.test import APITestCase
-from datetime import datetime
 
 
-class FreteiroTests(APITestCase):
-    def test_create_freteiro_sistema(self):
+class FreteiroTestsSistema(APITestCase):
+    def setUp(self) -> None:
+        endereco = Endereco.objects.create(
+            rua="Rua Teste",
+            CEP="12345678",
+            numero="123",
+            bairro="Bairro Teste",
+            cidade="Cidade Teste",
+            estado="Estado Teste",
+            complemento="Complemento Teste",
+        )
+        Freteiro.objects.create(
+            username="ArthurPaiva",
+            first_name="Arthur",
+            last_name="Paiva",
+            cpf="1234567",
+            email="testea@gmail.com",
+            endereco=endereco,
+        )
+        return super().setUp()
+
+    def test_create_freteiro(self):
         """
         Ensure we can create a new freteiro.
         """
@@ -32,6 +52,25 @@ class FreteiroTests(APITestCase):
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
         self.assertEqual(Freteiro.objects.filter(cpf=data["cpf"]).exists(), True)
 
+    def test_update_freteiro(self):
+        freteiro_inicial = Freteiro.objects.get(username="ArthurPaiva")
+        url = reverse("freteiro-detail", kwargs={"pk": freteiro_inicial.id})
+        self.client.force_authenticate(user=User.objects.get(username="ArthurPaiva"))
+        post_data = {"email": "novoemail@gmail.com"}
+        response = self.client.patch(url, post_data, format="json")
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(Freteiro.objects.filter(email=post_data["email"]).exists(), True)
+
+    def test_delete_freteiro(self):
+        freteiro_inicial = Freteiro.objects.get(username="ArthurPaiva")
+        url = reverse("freteiro-detail", kwargs={"pk": freteiro_inicial.id})
+        self.client.force_authenticate(user=User.objects.get(username="ArthurPaiva"))
+        response = self.client.delete(url, format="json")
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+        self.assertEqual(Freteiro.objects.filter(username="ArthurPaiva").exists(), False)
+
+
+class FreteiroTestsIntegracao(APITestCase):
     def test_create_freteiro_integracao(self):
         endereco = Endereco.objects.create(
             rua="Rua Teste",
@@ -42,20 +81,20 @@ class FreteiroTests(APITestCase):
             estado="Estado Teste",
             complemento="Complemento Teste",
         )
-        freteiro = Freteiro.objects.create(
+        Freteiro.objects.create(
             username="ArthurPaiva",
             first_name="Arthur",
             last_name="Paiva",
             cpf="12345678910",
             email="teste@gmail.com",
-            endereco=endereco
+            endereco=endereco,
         )
         freteiro_created = Freteiro.objects.filter(username="ArthurPaiva").exists()
         self.assertEqual(freteiro_created, True)
 
+
 class ClienteTestsSistema(APITestCase):
     def test_create_cliente(self):
-
         url = reverse("auth-register-cliente")
         data = {
             "full_name": "Mathews Dantas Bezerra dos Santos",
@@ -64,22 +103,20 @@ class ClienteTestsSistema(APITestCase):
             "password": "Tads.d.b.s123",
         }
         response = self.client.post(url, data, format="json")
-        print('JSON:', response.json())
-        print('STATUS: ', response.status_code)
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
         self.assertEqual(Cliente.objects.filter(cpf=data["cpf"]).exists(), True)
 
-        user = User.objects.get(username='cliente@hotmail.com')
+        user = User.objects.get(username="cliente@hotmail.com")
         # print(user)
         # client2 = APIClient()
         self.client.force_authenticate(user=user)
 
         url2 = reverse("cliente-list")
         response = self.client.get(url2, data=None, format=None)
-        print('CLIENTES: ',response.json())
+
 
 class ClienteTestsIntegracao(APITestCase):
-    def setUp(self):        
+    def setUp(self):
         data = {
             "username": "MathewsDantas",
             "first_name": "Mathews",
@@ -96,7 +133,6 @@ class ClienteTestsIntegracao(APITestCase):
         cliente.save()
 
         updated_cliente = Cliente.objects.get(id=self.cliente.id)
-        print(updated_cliente.email)
         self.assertEqual(updated_cliente.email, "cliente2@hotmail.com")
 
     def test_delete_cliente(self):
@@ -105,10 +141,10 @@ class ClienteTestsIntegracao(APITestCase):
 
         self.assertEqual(Cliente.objects.filter(email="cliente@hotmail.com").exists(), False)
 
+
 class CadastroTipoVeiculoTestsSistema(APITestCase):
     def test_create_tipo_veiculo(self):
-
-        #CRIANDO FRETEIRO
+        # CRIANDO FRETEIRO
         urlFreteiro = reverse("auth-register-freteiro")
         dataFreteiro = {
             "full_name": "Italo Gabriel Teste",
@@ -126,17 +162,17 @@ class CadastroTipoVeiculoTestsSistema(APITestCase):
             },
         }
         self.client.post(urlFreteiro, dataFreteiro, format="json")
-        #RECUPERA O USER CONTIDO NO FRETEIRO
-        user = User.objects.get(username='teste@gmail.com')
-        freteiro = Freteiro.objects.get(username='teste@gmail.com')
+        # RECUPERA O USER CONTIDO NO FRETEIRO
+        user = User.objects.get(username="teste@gmail.com")
+        freteiro = Freteiro.objects.get(username="teste@gmail.com")
 
-        #AUTENTICA O USER DO FRETEIRO
+        # AUTENTICA O USER DO FRETEIRO
         self.client.force_authenticate(user=user)
 
-        #CRIA O TIPO VEICULO
+        # CRIA O TIPO VEICULO
         urlTipoVeiculo = reverse("tipodeveiculo-list")
         dataTipoVeiculo = {
-          "descricao": "Caminhão 3/4",
+            "descricao": "Caminhão 3/4",
         }
 
         response = self.client.post(urlTipoVeiculo, dataTipoVeiculo, format="json")
@@ -163,10 +199,11 @@ class CadastroTipoVeiculoIntegracao(APITestCase):
         tipo_veiculo_exists = TipoVeiculo.objects.filter(descricao="caminhao 3/4").exists()
         self.assertFalse(tipo_veiculo_exists)
 
+
 # class EnderecoTests(APITestCase):
 #     def test_create_endereco(self):
 #         data = {
-#             'data_coleta' : '2023-06-22', 
+#             'data_coleta' : '2023-06-22',
 #             'data_entrega' : '2023-06-23',
 #             'turno_coleta' : 'TA',
 #             'turno_entrega' : 'NO'
@@ -174,67 +211,55 @@ class CadastroTipoVeiculoIntegracao(APITestCase):
 #         Endereco.objects.create(**data)
 #         self.assertEqual(Endereco.objects.filter(CEP="12345678").exists(), True)
 
-class PedidoTests(APITestCase):  
-    
+
+class PedidoTests(APITestCase):
     def setUp(self):
         self.cliente = Cliente.objects.create_user(
-            username='stark',
-            email='tonny@stark.com',
-            password='senha123S',
-            cpf='123456789'
+            username="stark", email="tonny@stark.com", password="senha123S", cpf="123456789"
         )
         self.endereco_origem = Endereco.objects.create(
-            rua='Rua Origem',
-            CEP='12345678',
-            numero='10',
-            bairro='Bairro Origem',
-            cidade='Cidade Origem',
-            estado='Estado Origem'
+            rua="Rua Origem",
+            CEP="12345678",
+            numero="10",
+            bairro="Bairro Origem",
+            cidade="Cidade Origem",
+            estado="Estado Origem",
         )
         self.endereco_destino = Endereco.objects.create(
-            rua='Rua Destino',
-            CEP='98765432',
-            numero='20',
-            bairro='Bairro Destino',
-            cidade='Cidade Destino',
-            estado='Estado Destino'
+            rua="Rua Destino",
+            CEP="98765432",
+            numero="20",
+            bairro="Bairro Destino",
+            cidade="Cidade Destino",
+            estado="Estado Destino",
         )
-        self.produto = Produto.objects.create(
-            nome='Mesa Gamer'
-        )
-        self.tipo_veiculo = TipoVeiculo.objects.create(
-            descricao='Caminhão'
-        )
-        #Modelo Principal
+        self.produto = Produto.objects.create(nome="Mesa Gamer")
+        self.tipo_veiculo = TipoVeiculo.objects.create(descricao="Caminhão")
+        # Modelo Principal
         self.pedido = Pedido.objects.create(
             cliente=self.cliente,
             origem=self.endereco_origem,
             destino=self.endereco_destino,
             produto=self.produto,
-            nomeDestinatario='Destinatário',
-            data_coleta='2023-06-21',
-            data_entrega='2023-06-22',
-            turno_entrega='MA',
-            turno_coleta='TA'
+            nomeDestinatario="Destinatário",
+            data_coleta="2023-06-21",
+            data_entrega="2023-06-22",
+            turno_entrega="MA",
+            turno_coleta="TA",
         )
         self.pedido.tipo_veiculo.add(self.tipo_veiculo)
 
     def test_create_pedido(self):
         self.assertEqual(Pedido.objects.all().count(), 1)  # Verifique se um novo pedido foi criado
-        self.assertEqual(Pedido.objects.last().nomeDestinatario, 'Destinatário')
+        self.assertEqual(Pedido.objects.last().nomeDestinatario, "Destinatário")
 
     def test_update_pedido(self):
-        data = {
-            'data_coleta' : '2023-06-22', 
-            'data_entrega' : '2023-06-23',
-            'turno_coleta' : 'TA',
-            'turno_entrega' : 'NO'
-        }
+        data = {"data_coleta": "2023-06-22", "data_entrega": "2023-06-23", "turno_coleta": "TA", "turno_entrega": "NO"}
         Pedido.objects.filter(id=self.pedido.id).update(**data)
         updated_pedido = Pedido.objects.get(id=self.pedido.id)
-        expected_data_coleta = datetime.strptime('2023-06-22', '%Y-%m-%d').date()
+        expected_data_coleta = datetime.strptime("2023-06-22", "%Y-%m-%d").date()
         self.assertEqual(updated_pedido.data_coleta, expected_data_coleta)
-        
+
     def test_delete_pedido(self):
         Pedido.objects.filter(id=self.pedido.id).delete()
         pedidos_restantes = Pedido.objects.all()
