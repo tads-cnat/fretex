654be1a7b305ecf507581d81889e8a4cf1a05f35
tonny-weslley <tonnyweslleyss@gmail.com>
2023-07-04 22:23:00 -0300

    test: finalizando teste de craição

--- backend/plataform/tests.py
+++ backend/plataform/tests.py
@@ -64,8 +64,8 @@ class ClienteTestsSistema(APITestCase):
             "password": "Tads.d.b.s123",
         }
         response = self.client.post(url, data, format="json")
-        print('JSON:', response.json())
-        print('STATUS: ', response.status_code)
+        # print('JSON:', response.json())
+        # print('STATUS: ', response.status_code)
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
         self.assertEqual(Cliente.objects.filter(cpf=data["cpf"]).exists(), True)
 
@@ -76,7 +76,7 @@ class ClienteTestsSistema(APITestCase):
 
         url2 = reverse("cliente-list")
         response = self.client.get(url2, data=None, format=None)
-        print('CLIENTES: ',response.json())
+        # print('CLIENTES: ',response.json())
 
 class ClienteTestsIntegracao(APITestCase):
     def setUp(self):        
@@ -96,7 +96,7 @@ class ClienteTestsIntegracao(APITestCase):
         cliente.save()
 
         updated_cliente = Cliente.objects.get(id=self.cliente.id)
-        print(updated_cliente.email)
+        # print(updated_cliente.email)
         self.assertEqual(updated_cliente.email, "cliente2@hotmail.com")
 
     def test_delete_cliente(self):
@@ -243,82 +243,61 @@ class PedidoTests(APITestCase):
         
 class PedidoTestsSistema(APITestCase):
     
-    def setUp(self):
-        self.cliente = Cliente.objects.create_user(
-            username='stark',
-            email='tonny@stark.com',
-            password='senha123S',
-            cpf='123456789'
-        )
-        self.endereco_origem = Endereco.objects.create(
-            rua='Rua Origem',
-            CEP='12345678',
-            numero='10',
-            bairro='Bairro Origem',
-            cidade='Cidade Origem',
-            estado='Estado Origem'
-        )
-        self.endereco_destino = Endereco.objects.create(
-            rua='Rua Destino',
-            CEP='98765432',
-            numero='20',
-            bairro='Bairro Destino',
-            cidade='Cidade Destino',
-            estado='Estado Destino'
-        )
-        self.produto = Produto.objects.create(
-            nome='Mesa Gamer'
-        )
-        self.tipo_veiculo = TipoVeiculo.objects.create(
-            descricao='Caminhão'
-        )
-        self.pedido = Pedido.objects.create(
-            cliente=self.cliente,
-            origem=self.endereco_origem,
-            destino=self.endereco_destino,
-            produto=self.produto,
-            nomeDestinatario='Destinatário',
-            data_coleta='2023-06-21',
-            data_entrega='2023-06-22',
-            turno_entrega='MA',
-            turno_coleta='TA'
-        )
-        self.pedido.tipo_veiculo.add(self.tipo_veiculo)
-
+   
     def test_create_pedido(self):
-        url = reverse('pedido_create') 
+        
+        urlCliente = reverse("auth-register-cliente")
         data = {
-            'cliente': self.cliente.id,
-            'origem': self.endereco_origem.id,
-            'destino': self.endereco_destino.id,
-            'produto': self.produto.id,
+            "full_name": "Tonny Weslley Silva de Souza",
+            "email": "tonny@cliente.com",
+            "cpf": "12312312311",
+            "password": "404401402",
+        }
+        
+        tipo_veiculo = TipoVeiculo.objects.create(descricao="caminhao 3/4")
+        tipo_veiculo2 = TipoVeiculo.objects.create(descricao="bike eletrica")
+
+        
+        response = self.client.post(urlCliente, data, format="json")
+        user = User.objects.get(username='tonny@cliente.com')
+        cliente = Cliente.objects.get(username='tonny@cliente.com')
+        
+        self.client.force_authenticate(user=user)
+    
+        
+    
+        data = {
+            'cliente': cliente.id,
+            'origem': {
+                "rua": "Rua Teste2",
+                "CEP": "12345679",
+                "numero": "123",
+                "bairro": "Bairro Teste",
+                "cidade": "Cidade Teste",
+                "estado": "Estado Teste",
+                "complemento": "Complemento Teste",
+            },
+            'tipo_veiculo' : [tipo_veiculo.id, tipo_veiculo2.id],
+            'destino': {
+                "rua": "Rua Teste3",
+                "CEP": "12345670",
+                "numero": "123",
+                "bairro": "Bairro Teste",
+                "cidade": "Cidade Teste",
+                "estado": "Estado Teste",
+                "complemento": "Complemento Teste",
+            },
+            'produto': {
+                "nome" : "cadeira gammer"    
+            },
             'nomeDestinatario': 'Destinatário',
             'data_coleta': '2023-06-21',
             'data_entrega': '2023-06-22',
             'turno_entrega': 'MA',
-            'turno_coleta': 'TA'
-        }
-        response = self.client.post(url, data, format="json")
-        self.assertEqual(response.status_code, status.HTTP_201_CREATED)  # Verifica se o pedido foi criado com sucesso
-        self.assertEqual(Pedido.objects.count(), 2)  # Verifica se o número total de pedidos é 2
-
-
-    def test_update_pedido(self):
-        url = reverse('pedido-detail', args=[self.pedido.id])
-        data = {
-            'data_coleta': '2023-06-22',
-            'data_entrega': '2023-06-23',
             'turno_coleta': 'TA',
-            'turno_entrega': 'NO'
         }
-        response = self.client.patch(url, data)
-        self.assertEqual(response.status_code, status.HTTP_200_OK)  # Verifica se a atualização do pedido foi bem-sucedida
-        self.pedido.refresh_from_db()  # Atualiza o objeto pedido do banco de dados
-        expected_data_coleta = datetime.strptime('2023-06-22', '%Y-%m-%d').date()
-        self.assertEqual(self.pedido.data_coleta, expected_data_coleta)
-
-    def test_delete_pedido(self):
-        url = reverse('pedido-detail', args=[self.pedido.id])  # Substitua 'pedido-detail' pelo nome da URL que corresponde à exclusão de um pedido
-        response = self.client.delete(url)
-        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)  # Verifica se a exclusão do pedido foi bem-sucedida
-        self.assertEqual(Pedido.objects.count(), 0)  # Verifica se o número total de pedidos é 0
+        
+        urlPedido = reverse("pedido-list")
+        response = self.client.post(urlPedido, data, format="json")
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)  # Verifica se o pedido foi criado com sucesso
+        self.assertEqual(Pedido.objects.count(), 1)  # Verifica se o número total de pedidos é 2
