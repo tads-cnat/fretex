83fc379dbd382c7d58acc8601db8e86f1bedf0d4
tonny-weslley <tonnyweslleyss@gmail.com>
2023-07-09 02:16:15 -0300

    feat: Adicionando Componente de email para criação de usuario

---
+++ frontend/src/pages/ResgisterUser/components/EmailCardForm/index.tsx
@@ -0,0 +1,103 @@
+import React, { useState, useEffect } from 'react';
+import { useForm, type SubmitHandler } from 'react-hook-form';
+import { yupResolver } from '@hookform/resolvers/yup';
+import { schemaUser } from '../../schemas';
+import { type IUserFormData } from '../../../../interfaces';
+import AuthService from '../../../../services/AuthService';
+import { toast } from 'react-toastify';
+import { ContainerPrincipal, ContainerForm, DivIcon } from './styles';
+import { Link, useNavigate } from 'react-router-dom';
+import { inputs } from './inputs';
+import { Button, Input, Logo } from '../../../../components';
+import { RiUserAddLine } from 'react-icons/ri';
+import { FaUserCircle } from 'react-icons/fa';
+
+
+export const EmailCardForm = () => {
+    const [error, setError] = useState<any>();
+    const [isLoading, setIsLoading] = useState<boolean>(false);
+    const {
+        register,
+        setValue,
+        handleSubmit,
+        formState: { errors },
+        setFocus,
+    } = useForm<IUserFormData>({
+        resolver: yupResolver(schemaUser),
+    });
+
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        setFocus('email');
+    }, [setFocus]);
+
+    const onSubmit: SubmitHandler<IUserFormData> = (data) => {
+        setError('');
+        setIsLoading(true);
+        const { email } = data;
+    };
+            
+
+    return (
+        console.log(errors["email"]),
+        <ContainerPrincipal>
+            <ContainerForm>
+                <DivIcon>
+                  <FaUserCircle color={'var(--bg-ligth)'} fontSize={'4rem'} />
+                </DivIcon>
+                <form onSubmit={handleSubmit(onSubmit)}>
+                
+                    <h1>Crie sua conta</h1>
+                    <div>
+
+                        {inputs.map((input, index) => (
+                            <Input
+                                key={index}
+                                {...register(`${input.name}`)}
+                                onChange={
+                                    input.onChange !== undefined
+                                      ? (e: React.ChangeEvent<HTMLInputElement>) => {
+                                          input.onChange(e, setValue);
+                                          setValue(`${input.name}`, e.target.value, {
+                                            shouldValidate: true,
+                                          });
+                                        }
+                                      : (e: React.ChangeEvent<HTMLInputElement>) => {
+                                          setValue(`${input.name}`, e.target.value, {
+                                            shouldValidate: true,
+                                          });
+                                        }
+                                  }
+                                type={input.type}
+                                name={input.name}
+                                label={input.label}
+                                placeholder={input.placeholder}
+                                required={input.required}
+                                register={register}
+                                error={errors[input.name]}
+                                svg={input.svg}
+                            />
+                        ))}
+                    </div>
+                    {error !== '' && <p className="error-light">{error}</p>}
+                    <section>
+                        <Button
+                        isButton
+                        Icon={RiUserAddLine}
+                        type="submit"
+                        isDisabled={isLoading}
+                        >
+                        Cadastre-se
+                        </Button>
+                        <p className='ajudaCadastro'>
+                        Já tem uma conta?<Link to="/login"> Entrar</Link>
+                        </p>
+                    </section>
+                </form>
+            </ContainerForm>
+        </ContainerPrincipal>
+
+    );
+
+}
